c8f0b0813518ae92eb7a890d5244af9b839e97d7
parkjihun22 <eyeful31@naver.com>
2026-07-31 15:40:53 +0900

    feat : 수정

--- src/components/InterestPopup/InterestPopup.jsx
+++ src/components/InterestPopup/InterestPopup.jsx
@@ -83,6 +83,20 @@ const InterestPopup = ({ onClose, registration, handleInputChange }) => {
                     required
                   />
                 </div>
+                <div className={styles.formGroup}>
+                <label>
+                  생년월일<span>*</span>
+                </label>
+                <input
+                  type="text"
+                  name="birth"
+                  value={registration.birth}
+                  onChange={handleInputChange}
+                  placeholder="예) 950101"
+                  maxLength={6}
+                  required
+                />
+              </div>
 
                 <button
                   type="submit"
